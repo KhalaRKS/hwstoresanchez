12e5122c390c1098a08b4aef1980e9087e4e895c
SanchoMazorka <lito_hx@hotmail.com>
2022-02-18 12:06:52 -0300

    cambios c3

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NavBar = () => {
+	return (
+		<>
+		<header>
+			<h1>hw e-commerce</h1>
+			<ul>
+				<li>Home</li>
+				<li>Productos</li>
+				<li>Ofertas</li>
+				<li>Mi cuenta</li>
+			</ul>
+		</header>
+		</>
+	)
+}
+
+export default NavBar;
