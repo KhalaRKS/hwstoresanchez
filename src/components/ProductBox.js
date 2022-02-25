7611aac7ba60f186b6205e91267e33a4cb69becb
SanchoMazorka <lito_hx@hotmail.com>
2022-02-25 01:00:29 -0300

    tarea clase 5 stock fix

--- src/components/ProductBox.js
+++ src/components/ProductBox.js
@@ -2,17 +2,18 @@ import React, { useState } from 'react'
 import ItemCount from './ItemCount'
 
 const ProductBox = () => {
-
 	const [state, setState] = useState( null )
+	const stock = parseInt(Math.random() * 5)
 
 	const cb = (dato) => { setState(dato) }
 
 	return (
 		<article className='nice-border shadow'>
-			<h5>iPhone 13 Pro</h5>
+			<span className='article--stock'>stock: <b>{stock}</b></span>
+			<h3>iPhone 13 Pro</h3>
 			<p>lorem</p>
 			<span>$1000</span>
-			<ItemCount stock={ parseInt(1 + Math.random() * 10) } initial={1} onAdd={cb} />
+			<ItemCount stock={ stock } initial={1} onAdd={cb} />
 			{state && <span onClick={ () => setState(null) } className='resultonadd'>Resultado onAdd: {state}</span>}
 		</article>
 	)
